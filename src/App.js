5ba426e413f81c4fd6ece37a9b460471c5721dc8
Limary Gonzalez <limaryg11@gmail.com>
2023-07-27 10:37:04 -0700

    removed header component as I implemented nav bar functionality and utlized roots instead

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SurfLocationList from './components/SurfLocationList';
 import SurfLocationDetails from './components/SurfLocationDetails';
 import AddSurfLocation from './components/AddSurfLocation';
-import Header from './components/Header';
 import Footer from './components/Footer';
 import Login from './components/Login';
 import Register from './components/Register';

7a0e0327d116b74218e032302baa9e1d5bc5bc93
Limary Gonzalez <limaryg11@gmail.com>
2023-08-07 14:45:12 -0700

    reorganizing app.js imports and trying to figure out how to reconfigure layout to incorporate bootstrap grid without losing functionality

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SurfLocationList from './components/SurfLocationList';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import SurfLocationDetails from './components/SurfLocationDetails';
 import AddSurfLocation from './components/AddSurfLocation';
 import Footer from './components/Footer';
+import SurfMap from './components/SurfMap';
 import Login from './components/Login';
 import Register from './components/Register';
 import './App.css';
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import SurfMap from './components/SurfMap';
 
 const API_URL = process.env.REACT_APP_API || 'http://localhost:8080';
 
